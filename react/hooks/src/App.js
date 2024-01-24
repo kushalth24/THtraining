23f6e42c1650fe9993fd76bd24f4d510e12971bb
codecolonel07 <kushalmpatel07@gmail.com>
2024-01-24 17:47:56 +0530

    useRef() hook Practice

--- react/hooks/src/App.js
+++ react/hooks/src/App.js
@@ -1,28 +1,32 @@
 
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 
-function slowFunction(num){
-  for(let i=0;i<1000000000;i++){}
-  return num*2;
-}
 
 function App() {
-  const [number, setNumber]=useState(0);
-  const [dark, setDark]=useState(false);
-  const doubleNumber=useMemo(()=>{
-    return slowFunction(number);
-  },[number]);
-  const themeStyles={
-    backgroundColor:dark ? 'black':'white',
-    color:dark ? 'white':'black'
-  }
+  const[name, setName]=useState("");
+  // const renderCount=useRef(1);
+  // useEffect(()=>{
+  //   renderCount.current=renderCount.current+1;
+  // })
+  //-----------------------------------------
+  // const inputRef=useRef();
+  // function focus(){
+  //   inputRef.current.focus();
+  // }
+  //-------------------------------------------abc
+  const prevName=useRef();
+  useEffect(()=>{
+    prevName.current=name;
+  },[name])
   return (
     <div className="App">
-      <input type='number' value={number} onChange={e=>setNumber(parseInt(e.target.value))}/>
-      <button onClick={()=>setDark(prevDark=>!prevDark)}>Change Theme</button>
-      <div style={themeStyles}>{doubleNumber}</div>
-      <h3>It will take longer time to double the number but not for changing theme. the page is rerendering everytime we change theme or double the number. but useMemo avoids the lengthy doubleNumber() function while changing theme</h3>
+      {/* <input ref={inputRef} value={name} onChange={e=>setName(e.target.value)}/> */}
+      <input value={name} onChange={e=>setName(e.target.value)}/>
+      {/* <div>My name is {name}</div> */}
+      {/* <div> I rendered {renderCount.current} times </div> */}
+      {/* <button onClick={focus}>Focus</button> */}
+      <div>My name is {name} and it used to be {prevName.current}</div>
     </div>
   );
 }
