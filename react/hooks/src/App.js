c93ba307d2c85d11c5708be632152c1647e5dc57
codecolonel07 <kushalmpatel07@gmail.com>
2024-01-25 09:09:52 +0530

    Custome Hook Practice: useLocalStorage hook.jsx added

--- react/hooks/src/App.js
+++ react/hooks/src/App.js
@@ -1,22 +1,19 @@
 
 import React, { useEffect, useMemo, useRef, useState, useCallback, useContext } from 'react';
 import './App.css';
-import FunctionComponentContext from './FunctionComponentContext';
+import FunctionComponentContext from './components/FunctionComponentContext';
+import useLocalStorage from './customHooks/useLocalStorage';
 
 export const ThemeContext=React.createContext();
 
 function App() {
-  const [darkTheme,setDarkTheme]=useState(true);
-  function toggleTheme(){
-    setDarkTheme(prevDarkTheme=> !prevDarkTheme)
-  }
+  const[name, setName]=useLocalStorage('name',"");
   return (
-    <div className="App">
-      <ThemeContext.Provider value={darkTheme}>
-        <button onClick={toggleTheme}>Toggle Theme</button>
-        <FunctionComponentContext/>
-      </ThemeContext.Provider>
-    </div>
+    <input
+    type='text'
+    value={name}
+    onChange={(e)=>{setName(e.target.value)}}
+    />
   );
 }
 
--- react/hooks/src/components/FunctionComponentContext.jsx
+++ react/hooks/src/components/FunctionComponentContext.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { ThemeContext } from './App';
+import { ThemeContext } from '../App';
 const FunctionComponentContext = () => {
     const dark=useContext(ThemeContext);
     const themeStyles={
